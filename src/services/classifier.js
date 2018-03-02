61120fccb5b0000b4f16a8299031f15022c42f08
Lee Lian Hoy <cruz@zyk3.net>
2018-03-02 17:34:56 +1300

    upgrade quantity matcher

--- src/services/classifier.js
+++ src/services/classifier.js
@@ -2,10 +2,14 @@ const fs = require('fs');
 const getMtgJson = require('mtg-json');
 
 function getCount(str) {
-  const matches = str.match(/(x\d|\dx|^\d)/g);
+  const matches = str.match(/((^|\s+)x\d($|\s+)|(^|\s+)\dx($|\s+)|^\d\s+)/g);
 
   return matches ? +(matches[0].replace('x', '')) : 1;
 }
+
+console.log(getCount('5 hello'));
+console.log(getCount('hello x6'));
+console.log(getCount('hello x7'));
 
 async function closestCardName(initialCardName) {
   const dataPath = `${__dirname}/../../data`;
